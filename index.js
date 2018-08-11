ccf433fdceaf203e4cde220c3bd6ef2830131fb5
Zach Deibert <zachdeibert@gmail.com>
2018-08-10 23:01:41 -0500

    Made generated hashes consistent

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const child_process = require("child_process");
 const fs = require("fs-extra");
+const ipfsAPI = require("ipfs-api");
 const npm = require("npm");
 const os = require("os");
 const path = require("path");
 const process = require("process");
 const recursive = require("recursive-readdir");
 
+let ipfs = ipfsAPI("/ip4/127.0.0.1/tcp/5001");
+
 function ipfsifyPackage(nodeModulesDir, packageName, packageMap, callback) {
-    fs.readJson(path.join(nodeModulesDir, packageName, "package.json"), (err, package_json) => {
+    let modDir = path.join(nodeModulesDir, packageName);
+    fs.readJson(path.join(modDir, "package.json"), (err, package_json) => {
         if (err) {
             callback(err);
         } else {
             let dependencies = package_json.dependencies ? Object.getOwnPropertyNames(package_json.dependencies) : [];
-            let submodulesDir = path.join(nodeModulesDir, packageName, "node_modules");
+            let submodulesDir = path.join(modDir, "node_modules");
             let packages = [];
-            function loop(i) {
+            let loop = i => {
                 if (i < dependencies.length) {
                     if (packageMap[dependencies[i]]) {
                         loop(i + 1);
                     } else {
-                        function loop2(dir) {
+                        let loop2 = dir => {
                             fs.access(path.join(dir, dependencies[i]), fs.constants.F_OK, err => {
                                 if (err) {
                                     let parent = path.dirname(path.dirname(dir));
@@ -45,67 +49,113 @@ function ipfsifyPackage(nodeModulesDir, packageName, packageMap, callback) {
                         loop2(submodulesDir);
                     }
                 } else {
-                    let modDir = path.join(nodeModulesDir, packageName);
                     recursive(modDir, (err, files) => {
                         if (err) {
                             callback(err, packages);
                         } else {
-                            files = files.filter(file => file.endsWith(".js"));
-                            function loop(i) {
+                            loop = i => {
                                 if (i < files.length) {
-                                    fs.readFile(files[i], {
-                                        "encoding": "utf8"
-                                    }, (err, data) => {
-                                        if (err) {
-                                            callback(err, packages);
-                                        } else {
-                                            let regex = /(require\s*\(\s*['"])([^'"]+)(['"]\s*\))/gm;
-                                            let match;
-                                            while (match = regex.exec(data)) {
-                                                if (packageMap[match[2]]) {
-                                                    data = data.substr(0, match.index) +
-                                                            match[1] +
-                                                            "/ipfs/" +
-                                                            packageMap[match[2]] +
-                                                            match[3] +
-                                                            data.substr(match.index + match[0].length);
+                                    if (files[i].endsWith(".js")) {
+                                        fs.readFile(files[i], {
+                                            "encoding": "utf8"
+                                        }, (err, data) => {
+                                            if (err) {
+                                                callback(err, packages);
+                                            } else {
+                                                let regex = /(require\s*\(\s*['"])([^'"]+)(['"]\s*\))/gm;
+                                                let match;
+                                                while (match = regex.exec(data)) {
+                                                    if (packageMap[match[2]]) {
+                                                        data = data.substr(0, match.index) +
+                                                                match[1] +
+                                                                "/ipfs/" +
+                                                                packageMap[match[2]] +
+                                                                match[3] +
+                                                                data.substr(match.index + match[0].length);
+                                                    }
                                                 }
+                                                fs.writeFile(files[i], data, {
+                                                    "encoding": "utf8"
+                                                }, err => {
+                                                    if (err) {
+                                                        callback(err, packages);
+                                                    } else {
+                                                        loop(i + 1);
+                                                    }
+                                                })
                                             }
-                                            fs.writeFile(files[i], data, {
-                                                "encoding": "utf8"
-                                            }, err => {
-                                                if (err) {
-                                                    callback(err, packages);
-                                                } else {
-                                                    loop(i + 1);
-                                                }
-                                            })
-                                        }
-                                    });
+                                        });
+                                    } else {
+                                        loop(i + 1);
+                                    }
                                 } else {
-                                    child_process.execFile("ipfs", [ "add", "-r", "-Q", "-H", modDir ], (err, hash, stderr) => {
+                                    package_json._where = "/npm-ipfsify";
+                                    fs.writeFile(path.join(modDir, "package.json"), JSON.stringify(package_json), {
+                                        "encoding": "utf8"
+                                    }, err => {
                                         if (err) {
                                             callback(err, packages);
-                                        } else if (stderr) {
-                                            callback(stderr, packages);
                                         } else {
-                                            hash = hash.trim()
-                                            packageMap[packageName] = hash;
-                                            packages.push({
-                                                "name": package_json.name,
-                                                "version": package_json.version,
-                                                "hash": hash
-                                            });
-                                            callback(null, packages);
+                                            let dataFiles = [];
+                                            let dirs = []
+                                            loop = i => {
+                                                if (i < files.length) {
+                                                    if (files[i].indexOf("node_modules", modDir.length) < 0) {
+                                                        fs.stat(files[i], (err, stats) => {
+                                                            if (err) {
+                                                                callback(err, packages);
+                                                            } else {
+                                                                if (stats.isDirectory()) {
+                                                                    dirs.push(files[i]);
+                                                                } else {
+                                                                    dataFiles.push(files[i]);
+                                                                }
+                                                                loop(i + 1);
+                                                            }
+                                                        });
+                                                    } else {
+                                                        loop(i + 1);
+                                                    }
+                                                } else {
+                                                    let stream = ipfs.files.addReadableStream();
+                                                    stream.on("error", err => {
+                                                        callback(err, packages);
+                                                    });
+                                                    stream.on("data", file => {
+                                                        if (file.path === "package") {
+                                                            packageMap[packageName] = file.hash;
+                                                            packages.push({
+                                                                "name": package_json.name,
+                                                                "version": package_json.version,
+                                                                "hash": file.hash
+                                                            });
+                                                            callback(null, packages);
+                                                        }
+                                                    });
+                                                    dataFiles.forEach(file => {
+                                                        stream.write({
+                                                            "path": path.join("package", file.substr(modDir.length + 1)),
+                                                            "content": fs.createReadStream(file)
+                                                        });
+                                                    });
+                                                    dirs.forEach(dir => {
+                                                        stream.write({
+                                                            "path": path.join("package", dir.substr(modDir.length + 1))
+                                                        });
+                                                    });
+                                                    stream.end();
+                                                }
+                                            };
+                                            loop(0);
                                         }
                                     });
                                 }
-                            }
+                            };
                             loop(0);
                         }
                     });
                 }
-            }
+            };
             loop(0);
         }
     });
